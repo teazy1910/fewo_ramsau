d00547ba9ff6559ed1aad1fb0388ce3aa7eddeb9
teazy1910 <tanjaisabel.dietz@gmail.com>
2023-06-26 10:41:26 +0200

    carousel

---
+++ components/Preise.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+    <h1>Some Content</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, velit
+      minima eveniet et laboriosam illum! Perspiciatis ducimus tenetur maiores
+      sed laudantium debitis iusto, vero dolores, velit magni soluta neque
+      explicabo.
+    </p>
+  </div>
+</template>
